30a9cc8152b6e6ba9a09c3fd801164e7e7d241aa
tobkrak <tobkrak@mail>
2016-12-20 21:43:53 +0100

    Utworzenie projektu

---
+++ src/main/java/eu/krakowiecki/DiabeticsApplication.java
@@ -0,0 +1,12 @@
+package eu.krakowiecki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabeticsApplication.class, args);
+	}
+}
---
+++ src/test/java/eu/krakowiecki/DiabeticsApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.krakowiecki;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiabeticsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
